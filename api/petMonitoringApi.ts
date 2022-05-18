55fc56a3293c89d9b4f1a014ec4bec5b89b21790
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2022-05-18 10:22:25 -0500

    Update petMonitoringApi.ts

--- api/petMonitoringApi.ts
+++ api/petMonitoringApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const petMonitoringApi = axios.create({
-  baseURL: "https://common-parrots-lick-157-100-91-151.loca.lt/",
+  baseURL: "https://pink-beers-kiss-157-100-91-151.loca.lt/",
 });
 
 export default petMonitoringApi;
