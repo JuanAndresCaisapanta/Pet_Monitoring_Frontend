3eced0c8895548b2bebea51e28e734a3a82862cf
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2022-05-26 14:03:46 -0500

    ancho main cambiado

--- components/ui/layout/MainContent.tsx
+++ components/ui/layout/MainContent.tsx
@@ -9,7 +9,7 @@ export const MainContent: FC<Props> = ({ children }) => {
    
     <Box
     component={'main'}
-      sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+      sx={{ flexGrow: 1, p: 1, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
     >
       <Toolbar />
       {children}
