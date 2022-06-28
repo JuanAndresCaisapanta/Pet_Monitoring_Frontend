595d8baade297fd4b8da9ec2e43ce92c0272aa0f
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2022-06-28 16:30:56 -0500

    arreglos middleware

--- pages/users/_middleware.ts
+++ pages/users/_middleware.ts
@@ -1,12 +1,17 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 import { IToken } from "../../interfaces";
-import jwt from "jsonwebtoken";
+import jwt_decode from "jwt-decode";
 export async function middleware(req: NextRequest | any, ev: NextFetchEvent) {
   const { token = "" } = req.cookies;
 
   const url = req.nextUrl.clone();
-  const data = jwt.decode(token) as IToken;
-  if (data?.role[0].authority.toString() === "ROLE_ADMIN" || !token) {
+  try {
+    const data = jwt_decode(token) as IToken;
+    if (data?.role[0].authority.toString() === "ROLE_ADMIN" || !token) {
+      url.pathname = "/auth/login";
+      return NextResponse.rewrite(url);
+    }
+  } catch (error) {
     url.pathname = "/auth/login";
     return NextResponse.rewrite(url);
   }
