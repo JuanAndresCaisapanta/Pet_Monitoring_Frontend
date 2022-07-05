0406234acf7f3886c5232e9b9bfd5c4db6f1b6a2
JuanAndresCaisapanta <juancaisapanta@hotmail.com>
2022-07-05 05:45:44 -0500

    Update TabManagement.tsx

--- components/ui/pets/TabManagement.tsx
+++ components/ui/pets/TabManagement.tsx
@@ -16,7 +16,7 @@ export const TabManagement = () => {
   const { id } = router.query;
 
   useEffect(() => {
-    getPet(id);
+    getPet(Number(id));
     return () => {
       petChange();
     };
